1156e400478a6da9caa27c336edf31bfcb3d8f6b
tks10 <tktktks10@gmail.com>
2018-08-30 20:25:19 +0900

    clean

--- main.py
+++ main.py
@@ -16,36 +16,28 @@ def load_dataset(train_rate):
 
 
 def train(parser):
-    # Get parameters
-    print(parser.gpu)
-    print(parser.epoch)
-    print(parser.batchsize)
-    print(parser.trainrate)
-    print(parser.augmentation)
-    print(parser.l2reg)
-
     # 訓練とテストデータを読み込みます
+    # Load train and test datas
     train, test = load_dataset(train_rate=parser.trainrate)
     valid = train.perm(0, 30)
     test = test.perm(0, 150)
 
+    # 結果保存用のインスタンスを作成します
     # Create Reporter Object
     reporter = rp.Reporter(parser=parser)
     accuracy_fig = reporter.create_figure("Accuracy", ("epoch", "accuracy"), ["train", "test"])
     loss_fig = reporter.create_figure("Loss", ("epoch", "loss"), ["train", "test"])
 
-    exit(0)
-
-    # Whether or not using a GPU
     # GPUを使用するか
+    # Whether or not using a GPU
     gpu = parser.gpu
 
-    # Create a model
     # モデルの生成
+    # Create a model
     model_unet = model.UNet(l2_reg=parser.l2reg).model
 
-    # Set loss function and optimizer
     # 誤差関数とオプティマイザの設定をします
+    # Set a loss function and an optimizer
     cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=model_unet.teacher,
                                                                            logits=model_unet.outputs))
     update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
@@ -53,18 +45,19 @@ def train(parser):
         train_step = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
 
     # 精度の算出をします
+    # Calculate accuracy
     correct_prediction = tf.equal(tf.argmax(model_unet.outputs, 3), tf.argmax(model_unet.teacher, 3))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    # Initialize session
     # セッションの初期化をします
+    # Initialize session
     gpu_config = tf.ConfigProto(gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=0.7), device_count={'GPU': 1},
                                 log_device_placement=False, allow_soft_placement=True)
     sess = tf.InteractiveSession(config=gpu_config) if gpu else tf.InteractiveSession()
     tf.global_variables_initializer().run()
 
-    # Train model
     # モデルの訓練
+    # Train the model
     epochs = parser.epoch
     batch_size = parser.batchsize
     is_augment = parser.augmentation
@@ -82,8 +75,8 @@ def train(parser):
             sess.run(train_step, feed_dict={model_unet.inputs: inputs, model_unet.teacher: teacher,
                                             model_unet.is_training: True})
 
-        # Evaluation
         # 評価
+        # Evaluation
         if epoch % 1 == 0:
             loss_train = sess.run(cross_entropy, feed_dict=train_dict)
             loss_test = sess.run(cross_entropy, feed_dict=test_dict)
@@ -108,8 +101,8 @@ def train(parser):
                 reporter.save_image_from_ndarray(train_set, test_set, train.palette, epoch,
                                                  index_void=len(ld.DataSet.CATEGORY)-1)
 
-    # Test trained model
     # 訓練済みモデルの評価
+    # Test the trained model
     loss_test = sess.run(cross_entropy, feed_dict=test_dict)
     accuracy_test = sess.run(accuracy, feed_dict=test_dict)
     print("Result")
@@ -124,12 +117,12 @@ def get_parser():
         add_help=True
     )
 
-    parser.add_argument('--gpu', action='store_true', help='Using GPUs')
-    parser.add_argument('--epoch', type=int, default=250, help='Number of epochs')
-    parser.add_argument('--batchsize', type=int, default=32, help='Batch size')
-    parser.add_argument('--trainrate', type=float, default=0.85, help='Training rate')
-    parser.add_argument('--augmentation', action='store_true', help='Number of epochs')
-    parser.add_argument('--l2reg', type=float, default=0.0001, help='L2 regularization')
+    parser.add_argument('-g', '--gpu', action='store_true', help='Using GPUs')
+    parser.add_argument('-e', '--epoch', type=int, default=250, help='Number of epochs')
+    parser.add_argument('-b', '--batchsize', type=int, default=32, help='Batch size')
+    parser.add_argument('-t', '--trainrate', type=float, default=0.85, help='Training rate')
+    parser.add_argument('-a', '--augmentation', action='store_true', help='Number of epochs')
+    parser.add_argument('-r', '--l2reg', type=float, default=0.0001, help='L2 regularization')
 
     return parser
 
