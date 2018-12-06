86ea11ad71e0e274df2c47f8afecb20657950e52
tks10 <tktktks10@gmail.com>
2018-12-06 19:01:41 +0900

    fix

--- util/repoter.py
+++ util/repoter.py
@@ -88,6 +88,8 @@ class Reporter:
             dst = Image.new("RGB", (im1.width + im2.width, im1.height))
             dst.paste(im1, (0, 0))
             dst.paste(im2, (im1.width, 0))
+        else:
+            raise NotImplementedError
 
         return dst
 
